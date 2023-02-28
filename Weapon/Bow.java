b5766606047a6d588f6c8accd37c011d61747686
AH1N <32333267+AH1N@users.noreply.github.com>
2023-03-01 00:00:17 +0300

    Delete Weapon directory
    ненужная

--- Weapon/Bow.java
+++
@@ -1,9 +0,0 @@
-package JavaGame.Weapon;
-
-public class Bow extends AbstractWeapon {
-
-    public Bow(int power, int range) {
-        super(power, range);
-        //TODO Auto-generated constructor stub
-    }
-}
